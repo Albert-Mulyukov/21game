8443e12d6ff3310767fee3ee157ffa57cbf4ec11
armulyukov <armulyukov02@mail.ru>
2019-05-02 18:26:38 +0300

    implemented the simplest variant of the game with only one player

--- Client.java
+++ Client.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.net.*;
 import java.io.*;
 import javax.imageio.ImageIO;
+import java.util.ArrayList;
 
 /* 
 <applet code="Client" width=1280 height=720>
@@ -16,13 +17,20 @@ import javax.imageio.ImageIO;
 public class Client extends JApplet implements ActionListener { 
 	private JButton button_connect;
 	private JButton button_disconnect;
+	private JButton new_game;
 	private JButton take_card;
+	private JButton show_cards;
 	private JTextField text;
+	private final int card_width = 200;
+	private final int card_height = 290;
+
 	private DataInputStream dis; 
     private DataOutputStream dos; 
     private Socket s;
     private boolean is_connected;
-    private Hand hand;
+    private boolean is_in_game;
+    private Hand my_hand;
+    private Hand bank_hand;
 
 	public void init() {
 		setLayout(new FlowLayout());
@@ -35,9 +43,17 @@ public class Client extends JApplet implements ActionListener {
         button_disconnect.addActionListener(this);
         add(button_disconnect);
 
+        new_game = new JButton("New game");
+        new_game.addActionListener(this);
+        add(new_game);
+
         take_card = new JButton("Take card");
         take_card.addActionListener(this);
         add(take_card);
+
+        show_cards = new JButton("Show cards");
+        show_cards.addActionListener(this);
+        add(show_cards);
 
         text = new JTextField();
         text.setText("Connect to server to start game");
@@ -45,7 +61,9 @@ public class Client extends JApplet implements ActionListener {
         add(text);
         
         is_connected = false;
-        hand = new Hand();
+        is_in_game = false;
+        my_hand = new Hand();
+        bank_hand = new Hand();
       	repaint();
 	}
 
@@ -67,29 +85,56 @@ public class Client extends JApplet implements ActionListener {
         g.clearRect(0, 0, width, height);
        
         int stride = 0;
-        if (hand.getCardsN() != 0) {
-        	stride = (width-100) / hand.getCardsN();
+        int card_base_x = 50;
+        if (my_hand.getCardsN() != 0) {
+        	stride = (width-100) / my_hand.getCardsN();
         }
-       	int card_base_x = 50;
-        for (int i = 0; i < hand.getCardsN(); i++) {
-        	g.drawImage(getCardImage(i), card_base_x, height/2, 250, 363, this);
+        for (int i = 0; i < my_hand.getCardsN(); i++) {
+        	g.drawImage(getCardImage(my_hand, i), card_base_x, height/2, card_width, card_height, this);
+        	card_base_x += stride;
+        }
+
+        stride = 0;
+        card_base_x = 50;
+        if (bank_hand.getCardsN() != 0) {
+        	stride = (width-100) / bank_hand.getCardsN();
+        }
+        for (int i = 0; i < bank_hand.getCardsN(); i++) {
+        	g.drawImage(getCardImage(bank_hand, i), card_base_x, 50, card_width, card_height, this);
         	card_base_x += stride;
         }
 	}
 
    	public void actionPerformed(ActionEvent e) {
-   		if ((e.getSource() == button_connect) && (is_connected == false)) {
+   		if ((e.getSource() == button_connect) && !is_connected) {
    			connect();
    			text.setText("You are connected to server");
    			repaint();
    		}
-   		else if ((e.getSource() == button_disconnect) && (is_connected == true)) {
+   		else if ((e.getSource() == button_disconnect) && is_connected) {
    			disconnect();
    			text.setText("Connect to server to start game");
    			repaint();
    		}
-   		else if ((e.getSource() == take_card) && (is_connected == true)) {
-   			hand.takeCard(cardRequest());
+   		else if ((e.getSource() == new_game) && is_connected) {
+   			newGameRequest();
+   			getBankCards();
+   			text.setText("");
+   			repaint();
+   		}
+   		else if ((e.getSource() == take_card) && is_connected && is_in_game) {
+   			my_hand.takeCard(cardRequest());
+   			repaint();
+   		}
+   		else if ((e.getSource() == show_cards) && is_connected && is_in_game) {
+   			boolean is_win = isWin();
+   			getBankCards();
+   			if (is_win) {
+   				text.setText("WIN! Bank score: " + bank_hand.getScore() + " You score: " + my_hand.getScore());
+   			}
+   			else {
+   				text.setText("LOSE! Bank score: " + bank_hand.getScore() + " You score: " + my_hand.getScore());
+   			}
    			repaint();
    		}
    	}
@@ -118,8 +163,21 @@ public class Client extends JApplet implements ActionListener {
 			dos.close();
 
 			is_connected = false;
+			is_in_game = false;
 		}
 		catch (Exception e) {
+  			e.printStackTrace();
+  		}
+   	}
+
+   	private void newGameRequest() {
+   		try {
+   			dos.writeUTF("New game");
+   			is_in_game = true;
+   			my_hand.clear();
+   			my_hand.takeCard(cardRequest());
+   		}
+   		catch (Exception e) {
   			e.printStackTrace();
   		}
    	}
@@ -128,7 +186,7 @@ public class Client extends JApplet implements ActionListener {
    		String suit = null;
    		String rank = null;
    		try {
-   			dos.writeUTF("Card Request");
+   			dos.writeUTF("Card request");
    			suit = dis.readUTF();
             rank = dis.readUTF();
    		}
@@ -138,7 +196,38 @@ public class Client extends JApplet implements ActionListener {
   		return new Card(suit, rank);
    	}
 
-   	private Image getCardImage(int index) {
+   	private boolean isWin() {
+   		boolean is_win = false;
+   		try {
+   			dos.writeUTF("Who wins");
+   			dos.writeInt(my_hand.getScore());
+   			is_win = dis.readBoolean();
+   			is_in_game = false;
+   		}
+   		catch (Exception e) {
+  			e.printStackTrace();
+  		}
+  		return is_win;
+   	}
+
+   	private void getBankCards() {
+   		bank_hand.clear();
+   		try {
+   			dos.writeUTF("Send cards");
+   			int n_cards = dis.readInt();
+   			String suit, rank;
+   			for (int i = 0; i < n_cards; i++) {
+   				suit = dis.readUTF();
+            	rank = dis.readUTF();
+            	bank_hand.takeCard(new Card(suit, rank));
+   			}
+   		}
+   		catch (Exception e) {
+  			e.printStackTrace();
+  		}
+   	}
+
+   	private Image getCardImage(Hand hand, int index) {
    		Image image = null;
 		label: switch (hand.getCard(index).getSuit()) {
 			case "C":
--- Server.java
+++ Server.java
@@ -5,7 +5,6 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 		Deck deck = new Deck();
 		deck.shuffle();
-		deck.shuffle();
 		ServerSocket ss = new ServerSocket(7777);
       	while (true) {
       		Socket s = null;
@@ -32,12 +31,14 @@ class ClientHandler extends Thread {
     private final DataOutputStream dos; 
     private final Socket s;
     private Deck deck;
+    private Hand bank_hand;
 
     public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos, Deck deck) {
     	this.s = s; 
         this.dis = dis; 
         this.dos = dos;
         this.deck = deck;
+        bank_hand = new Hand();
     }
 
     @Override
@@ -48,17 +49,45 @@ class ClientHandler extends Thread {
         try {
         	dos.writeUTF("Hello from server!");
         	loop: while (true) {
+        		Card card_tmp;
         		switch (dis.readUTF()) {
         			case "Exit":
         				System.out.println("Client " + s + " connection closed"); 
                     	s.close(); 
                     	break loop;
-                    case "Card Request":
-                    	Card card = deck.getCard();
-                    	String suit = card.getSuit();
-                    	String rank = card.getRank();
-                    	dos.writeUTF(suit);
-                    	dos.writeUTF(rank);
+                    case "New game":
+                    	deck = new Deck();
+                    	deck.shuffle();
+                    	bank_hand.clear();
+                    	bank_hand.takeCard(deck.getCard());
+                    	break;
+                    case "Card request":
+                    	card_tmp = deck.getCard();
+                    	dos.writeUTF(card_tmp.getSuit());
+                    	dos.writeUTF(card_tmp.getRank());
+                    	break;
+                    case "Send cards":
+                    	dos.writeInt(bank_hand.getCardsN());
+                    	for (int i = 0; i < bank_hand.getCardsN(); i++) {
+                    		card_tmp = bank_hand.getCard(i);
+                    		dos.writeUTF(card_tmp.getSuit());
+                    		dos.writeUTF(card_tmp.getRank());
+                    	}
+                    	break;
+                    case "Who wins":
+                    	boolean is_player_win = false;
+                    	int player_score = dis.readInt();
+                    	int bank_score = 99;
+                    	if (player_score <= 21) {
+                    		while (bank_hand.getScore() <= 16) {
+                    			bank_hand.takeCard(deck.getCard());
+                    		}
+                    		bank_score = bank_hand.getScore();
+                    		if ((bank_score > 21) || (player_score > bank_score)) {
+                    			is_player_win = true;
+                    		}
+                    	}
+                    	dos.writeBoolean(is_player_win);
                     	break;
                     default:
                     	System.out.println("Unknown command"); 
